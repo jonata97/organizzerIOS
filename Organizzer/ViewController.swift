df263d771e1d668e678fa46dd186b7bb4709b69c
Jonata Santos <jonata.correa@poatek.com>
2018-04-18 22:18:23 -0300

    Setting up the project

---
+++ Organizzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Organizzer
+//
+//  Created by Jonata Santos on 18/04/2018.
+//  Copyright © 2018 Jonata Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
